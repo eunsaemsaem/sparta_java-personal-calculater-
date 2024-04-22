86206b07cb6b96a5266f4ddcbe931154f7a10f9f
Robbie <robbie331@>
2024-04-22 20:12:36 +0900

    level 1-4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,32 +6,41 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int a = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int b = sc.nextInt();
+        while (true) {
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        /* scanner로 char을 입력받기 위해서는 형변환 필요 */
-        char op = sc.next().charAt(0); // 0 : 문자의 위치
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
 
-        int result = 0;
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            /* scanner로 char을 입력받기 위해서는 형변환 필요 */
+            char op = sc.next().charAt(0); // 0 : 문자의 위치
 
-        /* 조건을 switch로 나눔 (intelliJ 자동 정렬 기능) */
-        switch (op) {
-            case '+' -> result = a + b;
-            case '-' -> result = a - b;
-            case '/' -> {
-                if (b == 0) {
-                    System.out.println("두 번째 숫자로 0이 입력될 수 없습니다.");
-                } else {
-                    result = a / b;
+            int result = 0;
+
+            /* 조건을 switch로 나눔 (intelliJ 자동 정렬 기능) */
+            switch (op) {
+                case '+' -> result = a + b;
+                case '-' -> result = a - b;
+                case '/' -> {
+                    if (b == 0) {
+                        System.out.println("두 번째 숫자로 0이 입력될 수 없습니다.");
+                    } else {
+                        result = a / b;
+                    }
                 }
+                case '*' -> result = a * b;
             }
-            case '*' -> result = a * b;
-        }
 
-        System.out.println("결과: " + result);
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                break;
+            }
+        }
 
     }
 }
