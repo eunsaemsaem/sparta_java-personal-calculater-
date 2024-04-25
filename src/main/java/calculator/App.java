5acbf21ae0f2d72923df07f29921df78b4b8feb8
eunsaem <sweeet0308@gmail.com>
2024-04-25 19:49:13 +0900

    level 2-2. App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,8 @@
 package calculator;
+//2. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.
+//        - 연산 수행 역할은 Calculator 클래스가 담당합니다.
+//        - 연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장됩니다.
+//        - 소스 코드 수정 후에도 수정 전의 기능들이 반드시 똑같이 동작해야합니다.
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -20,6 +24,7 @@ public class App {
             //scanner로 char을 입력받기 위해서는 형변환 필요
             char op = sc.next().charAt(0); // 0 : 문자의 위치
 
+            /* Calculator 인스턴스 생성 */
             Calculator calculator = new Calculator();
             double result = 0;
             result = calculator.calculate(a, b, op);
@@ -30,14 +35,14 @@ public class App {
             resultList.add(result);
 
             /* 첫번째 결과 삭제 */
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제): ");
             String remove = sc.next();
             if (remove.equals("remove")) {
                 resultList.remove(0); //0번 결과 삭제
             }
 
             /* 결과 조회 */
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회): ");
             String inquiry = sc.next();
             if (inquiry.equals("inquiry")) {
                 for(double d:resultList) { //for-each문
@@ -46,7 +51,7 @@ public class App {
             }
 
             /* 반복 유무 확인 */
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             String exit = sc.next();
             if (exit.equals("exit")) {
                 break;
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,4 @@
 package calculator;
-//1. 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후
-//   결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
-//    - 나눗셈에서 분모에 0이 들어오거나 연산자 기호가 잘 못 들어온 경우
-//      적합한 Exception 클래스를 생성하여 throw 합니다. (매개변수로 해당 오류 내용을 전달합니다.)
-
-import java.io.IOError;
 
 public class Calculator {
     public double calculate (int a, int b, char op) throws CalculateException {
